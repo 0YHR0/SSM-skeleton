98502959c4f65a4d1cd24ce5aaf2a147920ce5e5
0YHR0 <97868579@qq.com>
2021-08-20 16:32:41 +0800

    First commit

--- src/main/java/com/yang/pojo/Student.java
+++ src/main/java/com/yang/pojo/Student.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * This is a student
+ * @author Yang Haoran
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
